b04d434954b47a23884b8d663f5bfeb22cde2494
unknown <za0162hid@gmail.com>
2023-04-03 16:09:13 +0600

    2 factor half design

--- src/pages/Security/TwoFA/index.jsx
+++ src/pages/Security/TwoFA/index.jsx
@@ -1,10 +1,52 @@
 import React from "react";
 import NavigationHeader from "../../../components/NavigationHeader";
+import { Box, Typography } from "@mui/material";
 
 function TwoFactorAuth() {
   return (
     <>
-      <NavigationHeader label="2fa" info />
+      <NavigationHeader label="2 Factor" info />
+
+      <Box sx={{
+          padding: "16px",
+        }}>
+          <Typography variant="h5">2-factor authentication</Typography>
+          <Typography
+          variant="body2"
+          sx={{ lineHeight: "22px", marginTop: "4px", marginBottom: "14px" }}
+        >
+          Select your 2-step verification method below.
+        </Typography>
+
+        <Box sx={{
+          padding: "16px",
+          display: "flex",
+          justifyContent: "space-between"
+        }}>
+          <Box>icon + text</Box>
+          <Box>button</Box>
+        </Box>
+
+        <Box sx={{
+          padding: "16px",
+          display: "flex",
+          justifyContent: "space-between"
+        }}>
+          <Box>icon + text</Box>
+          <Box>button</Box>
+        </Box>
+
+        <Box sx={{
+          padding: "16px",
+          display: "flex",
+          justifyContent: "space-between"
+        }}>
+          <Box>icon + text</Box>
+          <Box>button</Box>
+        </Box>
+      </Box>
+      
+
     </>
   );
 }
