2344fc35bcdaed5288ab55fcfdfdef5306d6a533
unknown <islamzihan435@gmail.com>
2023-04-07 18:48:19 +0600

    aligin ment

--- src/pages/Send/CheckAssets/index.jsx
+++ src/pages/Send/CheckAssets/index.jsx
@@ -13,12 +13,14 @@ function CheckAssets() {
       topValue: 0.028,
       bottomValue: 450.01,
       icon: <EthIcon width="18" height="26" />,
+      type: "ETH",
     },
     {
       label: "Uni",
       topValue: 123,
       bottomValue: 1450.01,
       icon: <UniIcon width="18" height="20" />,
+      type: "UNI",
     },
   ];
 
@@ -33,64 +35,68 @@ function CheckAssets() {
           flexDirection: "column",
         }}
       >
-        <Box
-          sx={{
-            color: theme.palette.text_colors.primary_550,
-            background: theme.palette.text_colors.neutral_0,
-            border: `1.56px solid ${theme.palette.key_colors.primary_475}`,
-            svgColor: theme.palette.text_colors.primary_550,
-            height: "60px",
-            borderRadius: "13px",
-            boxSizing: "border-box",
-            marginBottom: "8px",
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            paddingLeft: "16px",
-            paddingRight: "16px",
-          }}
-        >
-          <Box sx={{ display: "flex" }}>
-            <EthIcon />{" "}
-            <Typography
-              sx={{
-                height: "24px",
-                fontSize: "14px",
-                color: theme.palette.text_colors.neutral_800,
-                lineHeight: "24px",
-                weight: "400",
-                marginLeft: "5px",
-              }}
-            >
-              Etherum
-            </Typography>
-          </Box>
-          <Box>
-            <Typography
-              sx={{
-                height: "24px",
-                fontSize: "16px",
-                color: theme.palette.text_colors.neutral_800,
-                lineHeight: "24px",
-                weight: "400",
-              }}
-            >
-              0.254 ETH
-            </Typography>
+        {CheckAssetsData.map((asset) => (
+          <Box
+            key={asset.label}
+            sx={{
+              color: theme.palette.text_colors.primary_550,
+              background: theme.palette.text_colors.neutral_0,
+              border: `1.56px solid ${theme.palette.key_colors.primary_475}`,
+              svgColor: theme.palette.text_colors.primary_550,
+              height: "60px",
+              borderRadius: "13px",
+              boxSizing: "border-box",
+              marginBottom: "8px",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingLeft: "16px",
+              paddingRight: "16px",
+            }}
+          >
+            <Box sx={{ display: "flex" }}>
+              {asset.icon}
+              <Typography
+                sx={{
+                  height: "24px",
+                  fontSize: "14px",
+                  color: theme.palette.text_colors.neutral_800,
+                  lineHeight: "24px",
+                  weight: "400",
+                  marginLeft: "5px",
+                }}
+              >
+                {asset.label}
+              </Typography>
+            </Box>
 
-            <Typography
-              sx={{
-                height: "16px",
-                fontSize: "12px",
-                color: theme.palette.text_colors.neutral_625,
-                lineHeight: "16px",
-                weight: "400",
-              }}
-            >
-              0.254 USD
-            </Typography>
+            <Box sx={{ textAlign: "center" }}>
+              <Typography
+                sx={{
+                  height: "24px",
+                  fontSize: "16px",
+                  color: theme.palette.text_colors.neutral_800,
+                  lineHeight: "24px",
+                  weight: "400",
+                }}
+              >
+                {asset.topValue} {asset.type}
+              </Typography>
+
+              <Typography
+                sx={{
+                  height: "16px",
+                  fontSize: "12px",
+                  color: theme.palette.text_colors.neutral_625,
+                  lineHeight: "16px",
+                  weight: "400",
+                }}
+              >
+                {asset.bottomValue} USD
+              </Typography>
+            </Box>
           </Box>
-        </Box>
+        ))}
 
         <Button
           size="normal"
